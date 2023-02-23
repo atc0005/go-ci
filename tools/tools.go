de72553ff1083f9c7ab0b5121b164cb992cde790
Adam Chalkley <atc0005@users.noreply.github.com>
2023-02-23 05:23:00 -0600

    Update tracked tools for Dependabot PRs
    Update tools/go.mod and tools/go.sum:
    
    - add choffmeister/git-describe-semver
    - add goreleaser/nfpm
    - add tc-hib/go-winres
    - go mod tidy
    
    Including the go.sum file to make the Go toolchain happy.

--- tools/tools.go
+++ tools/tools.go
@@ -10,11 +10,14 @@ package tools
 //
 // nolint
 import (
+	_ "github.com/choffmeister/git-describe-semver"
 	_ "github.com/fatih/errwrap"
 	_ "github.com/golangci/golangci-lint/pkg/config"
+	_ "github.com/goreleaser/nfpm/v2"
 	_ "github.com/orijtech/httperroryzer"
 	_ "github.com/orijtech/structslop"
 	_ "github.com/pelletier/go-toml/v2"
+	_ "github.com/tc-hib/go-winres"
 	_ "golang.org/x/vuln/vulncheck"
 	_ "honnef.co/go/tools/config"
 )
